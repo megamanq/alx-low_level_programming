d45ba35a35444f9bd7c5d42156bfb492f7e00cb6
kamel <mega001@mail.com>
2023-06-15 18:59:05 -0700

    fic the 4-puts scritagaaain

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * this function to write a message using puts
+ *
+ * the return is always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
