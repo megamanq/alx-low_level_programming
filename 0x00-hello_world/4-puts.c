9bdd207ef9e9577b4d805127428be7c8cb6b605d
kamel <mega001@mail.com>
2023-06-16 08:51:11 -0700

    add the 4 putt c code fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  * description: this function write that sentence on the std out
  *
- * return: always 0 when succeed
+ * return: always (0) when succeed
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
