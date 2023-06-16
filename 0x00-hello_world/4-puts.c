e56c61b3cef7d6f240c139608de6d536eea28491
kamel <mega001@mail.com>
2023-06-16 08:47:10 -0700

    add the 4 putt c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdout.h>
+/**
+ * main: entry point
+ *
+ * description: this function write that sentence on the std out
+ *
+ * return: always 0 when succeed
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
