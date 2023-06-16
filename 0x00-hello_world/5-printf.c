412e134da5cde661b50170a50eaaa017432aef44
kamel <mega001@mail.com>
2023-06-15 19:16:32 -0700

    fic the 5- printf scriipt

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a sentence to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
