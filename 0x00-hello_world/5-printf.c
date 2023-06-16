94e0788d26964833ab75f7fae5187db01611be29
kamel <mega001@mail.com>
2023-06-16 09:04:03 -0700

    add the 5 printf c code fix betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  * main - entry point
  *
- * description: this code print that sentence on teh stndr out using printf.
+ * description : this code print that sentence on teh stndr out using printf.
  *
  * return 0 when on succeed
  */
-int main()
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
