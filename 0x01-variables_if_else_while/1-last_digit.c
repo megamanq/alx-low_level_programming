cebc74d8817914686a05462aaeb043120209dc03
kamel <mega001@mail.com>
2023-06-17 16:22:35 -0700

    add the nubr 1 fix3 and fak betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,18 +16,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = (n % 10);
-	if (last_digit < 5 && last_digit != 0)
+	last_dig = (n % 10);
+	if (last_dig < 5 && last_dig != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
+	} else if (last_dig == 0)
+	{
+		 printf("Last digit of %d is %d and is 0\n", n, last_dig);
+	} else
+	{
+		 printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
 	}
-		else if (last_digit == 0)
-		{
-			 printf("Last digit of %d is %d and is 0\n", n, last_digit);
-		}
-		else
-		{
-			 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-		}
 	return (0);
 }
