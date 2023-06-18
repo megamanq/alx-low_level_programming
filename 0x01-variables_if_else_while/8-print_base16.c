732208276d17daae376bd85c0327912b006c4426
kamel <mega001@mail.com>
2023-06-17 18:41:40 -0700

    add 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print all singl numbr on base 16 char methd
+ *
+ * Return: 0 when succeed
+ */
+int main(void)
+{
+	char a = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 'a';
+	for (i = 0; i < 6; i++)
+	{
+		putchar(a);
+		a++
+	}
+	putchar('\n');
+	return (0);
+}
