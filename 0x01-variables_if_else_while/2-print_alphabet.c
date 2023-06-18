f9b63357aea9f8eb7f39ac59384c36c01558e635
kamel <mega001@mail.com>
2023-06-17 17:01:27 -0700

    add 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * descripion: this code prints alphabets
+ *
+ * Return: is always 0 on succeed
+ */
+
+int main(void)
+{
+	char a = 0;
+
+	for (;a<z;a++)
+	{
+		putchar(a);
+		putchar("\n");
+	}
+	return (0);
+}
