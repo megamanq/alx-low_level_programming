2171eb193082561ad7211a3c3dda96e8427c10e4
kamel <mega001@mail.com>
2023-06-17 19:57:30 -0700

    add 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: combo print of two digit nms
+ *
+ * Return: alway returns 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = i + 1;
+		for (; j < 10; j++)
+		{
+			k = j + 1;
+			for (; k < 10;)
+			{
+				putchar('0' + i);
+                        	putchar('0' + j);
+				putchar('0' + k);
+				if (k != 9 || j != 8 || i != 7)
+				{
+                       		putchar(',');
+                        	putchar(' ');
+                        	}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
