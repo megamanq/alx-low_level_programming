c4b25f3669080891edcccecb12101da194559477
kamel <mega001@mail.com>
2023-06-17 18:01:35 -0700

    add 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print all singl numbr on base 10 char methd
+ *
+ * Return: 0 when succeed
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar( + i);
+	}
+	putchar('\n');
+	return (0);
+}
