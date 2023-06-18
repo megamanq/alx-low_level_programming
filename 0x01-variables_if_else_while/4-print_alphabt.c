7a9f23135d3885ba5af0055fd93695608c1d6ce5
kamel <mega001@mail.com>
2023-06-17 17:40:34 -0700

    add 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description : print all alfabet lower \q and e
+ *
+ * Return : is always 0 when succeed
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' || i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
