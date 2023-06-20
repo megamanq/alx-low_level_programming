4323c5d16807f835bfa3dc6306ab3092ebfc361f
kamel <mega001@mail.com>
2023-06-20 09:35:40 -0700

    add the 4 is alpha fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _isalpha : check if the entry data is alphabetic
+ * _isalpha - check if the entry data is alphabetic
  *
- * @c : is the data to be checked
+ * @c: is the data to be checked
  *
- * Return : 1 if c is a letter and 0 if not
+ * Return: 1 if c is a letter and 0 if not
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122)||(c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	} else
