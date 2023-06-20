32933d641e690e1929e4636f51bb434f259d0171
kamel <mega001@mail.com>
2023-06-20 08:59:45 -0700

    add the x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function
+ *
+ * descriptio: this code prints the alphabet in lowrcase x10
+ *
+ * Return: no return void
+ */
+
+void print_alphabet_x10(void)
+{
+	int k = 0;
+	while (k < 10)
+	{
+		char i;
+
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		 _putchar('\n');
+	k++;
+	}
+}
