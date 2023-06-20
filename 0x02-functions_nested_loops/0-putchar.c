46965915f2b73feccb263a109d10c6a407347c56
kamel <mega001@mail.com>
2023-06-20 06:52:31 -0700

    add first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * descriptio: this code prints putchar
+ *
+ * Return: on success 0
+ */
+int main(void)
+{
+	_putchar('a');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This read me file is for the 0x02 C Functions nested loops
