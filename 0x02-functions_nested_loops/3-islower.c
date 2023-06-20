b17c331125265d7174198120473b3630c0bd301a
kamel <mega001@mail.com>
2023-06-20 09:14:13 -0700

    add 3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function
+ *
+ * description: check if lower case or not
+ *
+ * Return: (1) if lowercase , else return (0)
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}else
+		return (0);
+}
