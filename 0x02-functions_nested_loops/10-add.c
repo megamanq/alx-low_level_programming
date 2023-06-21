3130220adf92f1232f0247552ca0bedd5d6aa684
kamel <mega001@mail.com>
2023-06-20 17:20:34 -0700

    add 10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - main function
+ *
+ * Description: add two integrs
+ * @a: first integer o add
+ * @b: second integer to add
+ *
+ * Return: sum of two integrs
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
