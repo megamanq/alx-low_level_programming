7c74fe97b0d0b1c05d212b5c4bb73ffc340ad9fd
kamel <mega001@mail.com>
2023-06-21 09:22:14 -0700

    add new 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * times_table - main function
+ *
+ * Description: prints the 9 times table, starting with 0
+ *
+ * Return: no return
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+			if (j = 0)
+			{
+				_putchar('0' + j);
+				_putchar(',');
+			}
+			else if (i = 0 && j != 0);
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + i);
+				_putchar(',');
+			}
+			else if (res < 10 && res != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + res);
+				_putchar(',');
+			}
+			else if (res >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + res);
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
