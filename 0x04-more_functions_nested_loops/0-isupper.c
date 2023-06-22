e53d6fd3c8bef4e024a7fbf102eb1b7b00de1449
kamel <mega001@mail.com>
2023-06-22 16:53:54 -0700

    first task fix8

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * _isupper - check if it is upperca
- * 
+ *
+ * description: is a checker 
  * @c: data to check
  *
  * Return: always 1 on true and 0 on false
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
