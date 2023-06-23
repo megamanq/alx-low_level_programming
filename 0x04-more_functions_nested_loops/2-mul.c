1dd7d71ea3af3d4736344128447b21f144481e79
kamel <mega001@mail.com>
2023-06-22 17:29:00 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - mul 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
