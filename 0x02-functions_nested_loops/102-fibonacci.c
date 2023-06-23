9a3086acf2594281b09fd7b5a45f9e7e81e6da5e
kamel <mega001@mail.com>
2023-06-23 07:42:37 -0700

    ad 102 fabionacciu

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print first 50 fibonacci
+ *
+ * Return: 0 on terminate
+ */
+int main(void)
+{
+	int i;
+	int a = 1;
+	int b = 2;
+	int res = 0;
+
+	for (i = 1; i <= 50; i++)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+		printf("%d, ", res);
+
+	}
+	printf("\n");
+	return (0);
+}
