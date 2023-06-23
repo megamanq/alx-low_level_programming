6d8ea0885c718bd3727f02cef8ef1073c52f5d82
kamel <mega001@mail.com>
2023-06-22 17:25:38 -0700

    task 0 add fix3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,18 @@
 #include "main.h"
+
 /**
- * _isupper - check if it is upperca
+ * _isupper - check letters
  *
- * description: is a checker 
  * @c: data to check
  *
- * Return: always 1 on true and 0 on false
+ * Return: 0 or 1
  */
-
 int _isupper(int c)
+
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
+
 }
