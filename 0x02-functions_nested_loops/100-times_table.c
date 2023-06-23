f68b2a2a7ba4c3cf5ddb46e86ffdd8a15295a8a6
kamel <mega001@mail.com>
2023-06-23 06:18:13 -0700

    add task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * print_times_table - print table n times
+ *
+ * @n: data entry
+ *
+ * Return: no return
+ */
+void print_times_table(int n)
+{
+	if (n < 15 && n > 0)
+	{
+		int i;
+		int j;
+		int res;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+				if (j == 0)
+				{
+					printf("%d", res);
+				}
+				else if (res < 10)
+				{
+					printf(",   %d", res);
+				}
+				else if (res < 100)
+				{
+					printf(",  %d", res);
+				}
+				else
+				{
+					printf(", %d", res);
+				}
+			}
+			printf("\n");
+		}
+	}
+}
