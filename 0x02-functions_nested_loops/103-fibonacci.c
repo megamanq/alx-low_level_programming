b11b924495c604e9f41e9d78d93d23ae886389bf
kamel <mega001@mail.com>
2023-06-23 08:35:51 -0700

    add 103 fabb

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print first 50 fibonacci
+ *
+ * Return: 0 on terminate
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+	unsigned long int a = 0;
+	unsigned long int b = 1;
+	unsigned long int res = 0;
+
+	for (i = 1; i <= 50; i++)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+		if (res <= 4000000)
+		{
+			sum += res;
+		}
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
