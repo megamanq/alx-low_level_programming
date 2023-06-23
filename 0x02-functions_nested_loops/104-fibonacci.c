cb1c64debff2e6555c5fa37fb917d79dea19b247
kamel <mega001@mail.com>
2023-06-23 08:47:24 -0700

    add 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print first 50 fibonacci
+ *
+ * Return: 0 on terminate
+ */
+int main(void)
+{
+	int i;
+	unsigned long int a = 0;
+	unsigned long int b = 1;
+	unsigned long int res = 0;
+
+	for (i = 1; i <= 50; i++)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+		if (i != 98)
+		{
+		printf("%lu, ", res);
+		}
+		else
+			printf("%lu", res);
+
+	}
+	printf("\n");
+	return (0);
+}
