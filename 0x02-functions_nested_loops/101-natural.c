abb1a43e32535fdb3b130f100dd268231209d222
kamel <mega001@mail.com>
2023-06-23 07:09:30 -0700

    add task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: sum of all multiples of 3 an 5
+ *
+ * Return: 0 when terminate program
+ */
+int main(void)
+{
+	int i;
+	int res = 0;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (!(i%3) || !(i%5))
+		{
+			res += i;
+		}
+	}
+	printf("%d\n", res);
+	return (0);
+}
