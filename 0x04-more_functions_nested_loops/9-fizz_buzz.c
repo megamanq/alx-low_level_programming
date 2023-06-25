cecf07bbc5e89ba70db01251600fb6f0df937108
kamel <mega001@mail.com>
2023-06-25 10:29:31 -0700

    add 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: fizz buzz test
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i != 1)
+		{
+			printf(" ");
+		}
+
+		if (!(i % 3) && !(i % 5))
+		{
+			printf("FizzBuzz");
+		}
+		else if (!(i % 3))
+		{
+			printf("Fizz");
+		}
+		else if (!(i % 5))
+		{
+			printf("Buzz");
+		}
+		else	
+			printf("%d", i);
+		
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
