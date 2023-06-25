e53bc0d262c5e889667449d63372002e7a215be0
kamel <mega001@mail.com>
2023-06-25 08:29:43 -0700

    add 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - main function
+ *
+ * description: print numbrs
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int i;
+
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar ('\n');
+}
