fd1cbfd3e5ccc2073ced03ec9dec7a3136064339
kamel <mega001@mail.com>
2023-06-26 14:59:56 -0700

    add 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - change value using pointer
+ *
+ * @n: pointer to an int
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers and arrays and string readme file of the repo
