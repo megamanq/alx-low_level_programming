304e9a2fb9a8851c1dc2b4e5607f7eda469ae5e4
kamel <mega001@mail.com>
2023-07-02 04:53:38 -0700

    add 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - main function
+ *
+ * description: take a pointer as prm and update its value
+ * @*n: pointer to an int
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
