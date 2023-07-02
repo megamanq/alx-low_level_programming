dbbc49194075686942149075463a976c8fc1a8d6
kamel <mega001@mail.com>
2023-07-02 06:16:16 -0700

    add 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * print_rev - main function
+ *
+ * description: print string in reverse
+ * @s: pointer to a string
+ *
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s); i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
