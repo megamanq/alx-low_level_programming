05e7796ed99cfe475e65a2c04c7f86975b79a6a0
kamel <mega001@mail.com>
2023-07-08 10:22:30 -0700

    add 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string - capitalise each word
+ * @x: string to capitalize
+ * Return: capitalized x
+ */
+char *cap_string(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] == ' ' || x[i] == '\t' || x[i] == '\n' ||
+            		x[i] == ',' || x[i] == ';' || x[i] == '.' ||
+            		x[i] == '!' || x[i] == '?' || x[i] == '"' ||
+            		x[i] == '(' || x[i] == ')' || x[i] == '{' || x[i] == '}')
+		{
+			if (x[i + 1] <= 122 && x[i + 1 ] >= 97)
+			{
+				x[i + 1] = (x[i + 1] - 32);
+			}
+		}
+		i++;
+	}
+	return (x);
+}
