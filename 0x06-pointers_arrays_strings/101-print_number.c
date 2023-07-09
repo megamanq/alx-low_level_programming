de4d42bbfad3383e9f8b46aceb92855a211da931
kamel <mega001@mail.com>
2023-07-09 11:06:49 -0700

    add 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_number - print numbers
+ * @n: nbr to print
+ * Return: no return
+ */
+void print_number(int n)
+{
+	if (!n)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		int m, i;
+		int div = 1;
+
+		if (n < 0)
+		{
+			_putchar('-');
+			n *= -1;
+			m = n;
+		}
+		else
+		{
+			m = n;
+		}
+
+		while (m > 9)
+		{
+			div *= 10;
+			m /= 10;
+		}
+
+		while (n)
+		{
+			i = n / div;
+			n %= div;
+			div /= 10;
+			_putchar('0' + i);
+		}
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *);
 char *leet(char *);
+int _putchar(char c);
