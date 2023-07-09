60ebf0f563884d4d619b40155999b644bcf4ee7e
kamel <mega001@mail.com>
2023-07-09 06:38:25 -0700

    add 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encoe str to 1337
+ * @str: str to encod
+ * Return: str
+ */
+char *leet(char *str)
+{
+	char letters[10] = "aAeEoOtTlL",
+	     codes[5] = "43071";
+	int j, i = 0;
+
+	while (str[i])
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				int c = j / 2;
+				str[i] = codes[c];
+			}
+		}
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *);
+char *leet(char *);
