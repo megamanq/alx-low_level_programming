79e8c425ffd2beb4f2d257bfdf52ac8b48e5154b
kamel <mega001@mail.com>
2023-07-09 09:42:51 -0700

    add 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,17 +6,20 @@
  */
 char *rot13(char *str)
 {
-	char alf[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		rot[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	int j, i = 0;
+	char alf[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0;
 
 	while (str[i])
 	{
-		for (j = 0; j < 52; j++)
+		int j;
+
+		for (j = 0; alf[j] != '\0'; j++)
 		{
 			if (str[i] == alf[j])
 			{
 				str[i] = rot[j];
+				break;
 			}
 		}
 		i++;
