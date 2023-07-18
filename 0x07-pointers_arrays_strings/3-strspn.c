52663973fed2851dd2471b04ca4339d20480db44
kamel <mega001@mail.com>
2023-07-18 07:52:28 -0700

    add 3 strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - get the lengh of prefix
+ * @s: string to trait
+ * @accept: segment to search
+ *
+ *Return: lenght
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		int j = 0;
+
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				j++;
+				break;
+			}
+			else return (i);
+		}
+	}
+	return (i);
+}
