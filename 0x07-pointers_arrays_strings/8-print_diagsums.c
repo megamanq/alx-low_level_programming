1c7aa17370fc0f8c0ff2682b0ad8133a11c7c40c
kamel <mega001@mail.com>
2023-07-18 12:23:35 -0700

    add 88

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,15 +9,14 @@
 void print_diagsums(int *a, int size)
 {
 	int i, sum_r = 0, sum_l = 0;
-	int *b = a;
-
+	
 	for (i = 0; i < size; i++)
 	{
-		sum_r += b[i * size + 1];
+		sum_r += a[i * size + 1];
 	}
 	for (i = size - 1; i >= 0; i--)
 	{
-		sum_l += b[i * size + (size - i - 1)];
+		sum_l += a[i * size + (size - i - 1)];
 	}
 	printf("%d, %d\n", sum_r , sum_l);
 }
