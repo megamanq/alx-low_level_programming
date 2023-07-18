94cf9567effe948d35b288251211da7e4a550ca3
kamel <mega001@mail.com>
2023-07-18 09:38:02 -0700

    add 3 fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,22 +8,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	while (s[i])
 	{
-		int j = 0;
+		int match = 0, j;
 
-		while (accept[j])
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (accept[j] == s[i])
 			{
-				j++;
+				match = 1;
 				break;
 			}
-			else if (accept[j + 1] == '\0')
-				return (i);
 		}
+		if (!match)
+		{
+			break;
+		}
+		i++;
 	}
 	return (i);
 }
