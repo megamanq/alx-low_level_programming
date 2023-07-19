667fd57e43973fca97e4a35f443bbd3df5faef45
kamel <mega001@mail.com>
2023-07-19 10:04:44 -0700

    add 0

--- 0x08-recursion/old/0-puts_recursion.c
+++ 0x08-recursion/old/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * _puts_recursion - print str
- * @s: str to print
- * Return: no return
- */
+  * _puts_recursion - prnt string
+  * @s: str to prnt
+  *
+  * Return: nor return
+  */
 void _puts_recursion(char *s)
 {
-	if (!(*s))
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+
 	_putchar(*s);
 	s++;
 	_puts_recursion(s);
