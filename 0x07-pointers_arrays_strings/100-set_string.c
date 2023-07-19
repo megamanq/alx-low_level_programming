8600f02d01fff68981b630539a7ed889f346eb5f
kamel <mega001@mail.com>
2023-07-19 06:44:19 -0700

    add 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of ptr to a char
+ * @s: ptr to ptr that will chng
+ * @to: ptr to the src of chng
+ * Return: no return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
