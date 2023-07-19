b407681f54a23e5a6a45f142f9c473bb260280ce
kamel <mega001@mail.com>
2023-07-19 06:01:30 -0700

    add 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - add
+ * @a: int
+ * @b: int
+ * Return: res
+ */
+int op_add(int a, int b)
+	{
+		return (a + b);
+	}
+
+/**
+ * op_sub - sub
+ * @a: int
+ * @b: int
+ * Return:res
+ */
+
+int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
+/**
+ * op_mul - mul
+ * @a: int
+ * @b: int
+ * Return: res
+ */
+
+int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+/**
+ * op_div - div
+ * @a: inte
+ * @b: int
+ * Return: res
+ */
+
+int op_div(int a, int b)
+	{
+		if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+		return (a / b);
+	}
+/**
+ * op_mod - mod
+ * @a: int
+ * @b: int
+ * Return: res
+ */
+
+int op_mod(int a, int b)
+	{
+		if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+		return (a % b);
+	}
