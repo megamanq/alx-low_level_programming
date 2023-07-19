a782c99ada5e73c78a7fb77c81a76e5fe3aa24c2
kamel <mega001@mail.com>
2023-07-19 12:32:12 -0700

    add 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * searchsqrt - search the square root
+ * @a:int
+ * @b:int
+ * Return: int
+ */
+
+int searchsqrt(int a, int b)
+{
+	if ((a * a) > b)
+		return (-1);
+
+	if ((a * a) == b)
+		return a;
+
+	return (searchsqrt ((a + 1), b));
+}
+
+/**
+ * _sqrt_recursion - main func , get the sqrt
+ * @n :int
+ * Return: sqrt root or -1 if error
+ */
+int _sqrt_recursion(int n)
+{
+	int a = 1;
+
+	return (searchsqrt(a, n));
+}
