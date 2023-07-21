2855d369c65c7e1f7a873651241ea7abc6ee2e22
kamel <mega001@mail.com>
2023-07-21 07:01:39 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/** main - print the prog name
+/**
+ * main - print the prog name
  * @argc: argument count
  * @argv: array of potr to char arguments
  * Return: 0 on sucess
  */
+
 int main(int argc __attribute__ ((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
