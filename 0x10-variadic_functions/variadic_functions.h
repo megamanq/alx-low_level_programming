0e6fe520584bfaf5bd4868bca78e868053481fa5
kamel <mega001@mail.com>
2023-07-21 11:20:57 -0700

    add 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef _FUNCTION_VARIADIC_H_
+#define _FUNCTION_VARIADIC_H_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#include<stdarg.h>
+/**
+ * struct print - mult choice print
+ * @x: char Type print
+ * @T_func: function
+ */
+typedef struct print
+{
+	char *x;
+	void (*T_func)(va_list);
+} t_print;
+
+#endif
