4c4a080af45d1b7941ab8d974f1c62cf3ca57cf0
kamel <mega001@mail.com>
2023-07-26 12:44:22 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - make a 2D array of ints
+ * @width: width of arr
+ * @height: height of arr
+ * Return: ptr to 2d arra or null on fail
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **arr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	arr = (int **)malloc(sizeof(int) * height);
+
+	if (!arr)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height ; i++)
+	{
+		arr[i] = (int *)malloc(sizeof(int) * width);
+		if (!arr[i])
+		{
+			for ( j = 0; j <= i; j++)
+			{
+				free(arr[j]);
+			}
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for ( j = 0; j < width ; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
