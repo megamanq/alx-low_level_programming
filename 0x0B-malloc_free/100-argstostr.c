2a716bdaefdd9e64239d72fc1a518fb1b4319217
kamel <mega001@mail.com>
2023-07-27 07:41:55 -0700

    fix - 5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - concatenates all args
+ * @arg: count
+ * @av: arg values
+ * return: pointer to new string or null if fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *buffer;
+	int i, j, buf_ind, len;
+
+	if (ac == 0 || *av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, len = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+
+		len++;
+	}
+
+	buffer = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (!buffer)
+	{
+		return (NULL);
+	}
+
+	buffer[0] = '\0';
+	buf_ind = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			buffer[buf_ind++] = av[i][j];
+		}
+
+		buffer[buf_ind++] = '\n';
+	}
+	buffer[buf_ind] = '\0';
+
+	return (buffer);
+}
