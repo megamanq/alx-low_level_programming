74d788e0632a90092ae69e3d997912ed4ac87a93
kamel <mega001@mail.com>
2023-07-27 16:04:21 -0700

    add100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - impl of myStartupFun
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
