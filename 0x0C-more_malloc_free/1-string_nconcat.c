38e645a70d5d83bf51b674a18beba9bce93c4cd4
kamel <mega001@mail.com>
2023-07-30 10:16:34 -0700

    fix betty style - 1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,54 +17,36 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = len2 = i = j = 0;
 
 	if (s1 == NULL)
-	{
 		len1 = 0;
-	}
 	else
 	{
 		while(s1[len1])
-		{
 			len1++;
-		}
 	}
 
 	if (s2 == NULL)
-	{
 		len2 = 0;
-	}
 	else
 	{
 		while (s2[len2])
-		{
 			len2++;
-		}
 	}
 
 	if (n >= len2)
-	{
 		all_len = len1 + len2;
-	}
 	else
-	{
 		all_len = len1 + n;
-	}
 
 	buffer = malloc(sizeof(char) * (all_len + 1));
 
 	if (!buffer)
-	{
 		return(NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		buffer[i] = s1[i];
-	}
 
 	for (j = 0; i < all_len; j++)
-	{
 		buffer[i++] = s2[j];
-	}
 
 	buffer[i] = '\0';
 
