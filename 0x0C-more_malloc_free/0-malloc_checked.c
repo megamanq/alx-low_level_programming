263f1060fb6c814f6ed33ed72be779a3e1fe0141
kamel <mega001@mail.com>
2023-07-30 08:17:41 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size to allocate
+ * Return: 98 if fail, ptr to memory if succed
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *buffer;
+
+	buffer = malloc(b);
+
+	if (!buffer)
+	{
+		exit (98);
+	}
+
+	return (buffer);
+}
