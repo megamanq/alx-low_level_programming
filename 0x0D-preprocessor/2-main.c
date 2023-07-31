780e3fa0d368a895fbfeabf1949ae43cf1037e5f
kamel <mega001@mail.com>
2023-07-31 15:35:03 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the eprgrrm name
+ * @argcc: argument count
+ * @argv: array of arrguments
+ * Return: no return
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
