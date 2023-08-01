2f178c149803d7520488647fe607737ca815d32a
kamel <mega001@mail.com>
2023-08-01 16:41:25 -0700

    fix - 4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * print_dog - print struct dog
- * @d: name of variable
+ * @d: pointer to struc
  * Return: no return
  */
 
@@ -12,16 +12,28 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
+		{
 			printf("Name: %s\n", d->name);
+		}
 		else
+		{
 			printf("Name: (nil)\n");
+		}
 		if (d->age)
+		{
 			printf("Age: %.6f\n", d->age);
+		}
 		else
+		{
 			printf("Age: (nil)\n");
+		}
 		if (d->owner)
+		{
 			printf("Owner: %s\n", d->owner);
+		}
 		else
+		{
 			printf("Owner: (nil)\n");
+		}
 	}
 }
