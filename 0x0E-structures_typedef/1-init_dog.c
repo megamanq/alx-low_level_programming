7e98a786581e9117eeca4cc2000e44ec7d4850bc
kamel <mega001@mail.com>
2023-08-01 15:39:49 -0700

    1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable de typoe dog
+ * @d: var name
+ * @name: fst memb of struct
+ * @age: scnd memb
+ * @owner: thrd memb
+ * Return: no return
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	(*d).owner = owner;
+}
