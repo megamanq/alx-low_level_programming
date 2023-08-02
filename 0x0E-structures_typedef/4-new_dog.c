a3a89e7fb2880115398edd52fff92dbc5693bafd
kamel <mega001@mail.com>
2023-08-01 17:43:39 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
 
+int strleng(char *str);
+char *strcp(char *dest, char *src);
+
 /**
  * new_dog - creates a new dog
  * @name: name of dog
@@ -12,16 +15,64 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *newdog;
 
-	dog = malloc(sizeof(dog_t));
+	newdog = malloc(sizeof(dog_t));
 
-	if (!dog)
-		return (NULL);
+	if (!name || !newdog || !owner)
+		return (0);
 
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	newdog->name = malloc(sizeof(char) * (strleng(name) + 1));
+	newdog->owner = malloc(sizeof(char) * (strleng(owner) + 1));
+	newdog->age = age;
 
-	return (dog);
+	if (!newdog->name || !newdog->owner)
+	{
+		free(newdog->owner);
+		free(newdog->name);
+		free(newdog);
+		return (0);
+	}
+
+	newdog->name = strcp(newdog->name, name);
+	newdog->owner = strcp(newdog->owner, owner);
+	newdog->age = age;
+
+	return (newdog);
+}
+
+/**
+ * strleng - get str lenght
+ * @str: ptr to string
+ * Return: nbr of chars
+ */
+
+int strleng(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+char *strcp(char *dest, char *src)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
