30f90bf5fb2536866c8fddc0a5767bd45703bf92
kamel <mega001@mail.com>
2023-08-01 18:24:20 -0700

    add6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delet head of node
+ * @head: dbl ptr
+ *Return: no return
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *strt;
+	int i;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	strt = *head;
+	*head = strt->next;
+	i = strt->i;
+	free(strt);
+
+	return (i);
+}
