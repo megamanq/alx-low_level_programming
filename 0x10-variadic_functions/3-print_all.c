8a899d84be63c007429b824b5ff3c4c0ca28b873
kamel <mega001@mail.com>
2023-08-06 05:53:49 -0700

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,99 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_c - print charecter
+ * @str: arg
+ * Return: no return
+ */
+
+void print_c(va_list str)
+{
+	printf("%c", va_arg(str, int));
+}
+
+/**
+ * print_str - print strings
+ * @str: arg
+ * Return: no return
+ */
+
+void print_str(va_list str)
+{
+	char *x;
+
+	x = va_arg(str, char*);
+
+	if (x == NULL)
+		x = "(nil)";
+
+	printf("%s", x);
+}
+
+/**
+ * print_int - print integers
+ * @str: arg
+ * Return: no return
+ */
+
+void print_int(va_list str)
+{
+	printf("%d", va_arg(str, int));
+}
+
+/**
+ * print_f - print floats
+ * @str: arg
+ * Return: no return
+ */
+
+void print_f(va_list str)
+{
+	printf("%f", va_arg(str, double));
+}
+
+/**
+ * print_all - print everything
+ * @format: list of types
+ * Return: no return
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	t_prin f[] = {
+		{"c", print_c},
+		{"s", print_str},
+		{"i", print_int},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list args;
+	char *sep = "";
+
+	va_start(args, format);
+	i = 0;
+
+	while (format[i])
+	{
+		j = 0;
+
+		while (f[j].y)
+		{
+			if (*(f[j].y) == format[i])
+			{
+				printf("%s", sep);
+				f[j].func(args);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,15 +6,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #include<stdarg.h>
+
 /**
  * struct print - mult choice print
  * @x: char Type print
  * @T_func: function
  */
-typedef struct print
+
+typedef struct prin
 {
-	char *x;
-	void (*T_func)(va_list);
-} t_print;
+	char *y;
+	void (*func)(va_list);
+} t_prin;
 
 #endif
