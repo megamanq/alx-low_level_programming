fe30c00b26e14725c9607b0dd11cf220e7ce8311
kamel <mega001@mail.com>
2023-08-11 14:40:28 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - creates file
+ * @filename: file path
+ * @text_content: content to write
+ * Return: -1 if error or 1 on success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, len;
+	ssize_t writen;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+
+		while (text_content[len])
+		{
+			len++;
+		}
+
+		writen = write(file, text_content, len);
+
+		if (writen == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	return (1);
+}
