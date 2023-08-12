9307dce0d4992a05ef3179991aa15b4b204c4ae2
kamel <mega001@mail.com>
2023-08-11 17:51:19 -0700

    fix 3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,14 +25,14 @@ void copi(int fdsrc, int fddest, char *fnsrc, char *fndest)
 		writen = write(fddest, buffer, reads);
 		if (writen == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", fndest);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fndest);
 			exit(99);
 		}
 	}
 
 	if (reads == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", fnsrc);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fnsrc);
 		exit(98);
 	}
 }
@@ -50,35 +50,35 @@ int main(int ac,  char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	file_src = open(av[1], O_RDONLY);
 	if (file_src == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
 	file_dest = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (file_dest == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 
 	copi(file_src, file_dest, av[1], av[2]);
 
 	fc1 = close(file_src);
-	if (fc1 < 0)
+	if (fc1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_src);
 		return (100);
 	}
 
 	fc2 = close(file_dest);
-	if (fc2 < 0)
+	if (fc2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_dest);
 		return (100);
