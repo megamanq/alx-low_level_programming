ab5e4336546707b48626c454435b1d92e5462e6a
kamel <mega001@mail.com>
2023-08-11 18:45:09 -0700

    add 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,36 +6,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void validate_elf(unsigned char *elf_id);
-void p_mag(unsigned char *elf_id);
-void p_cl(unsigned char *elf_id);
-void p_data(unsigned char *elf_id);
-void p_vers(unsigned char *elf_id);
-void p_abi(unsigned char *elf_id);
-void p_osab(unsigned char *elf_id);
-void p_type(unsigned int elf_type, unsigned char *elf_id);
-void p_entr(unsigned long int elf_ent, unsigned char *elf_id);
+void validate_elf(unsigned char *e_ident);
+void p_mag(unsigned char *e_ident);
+void p_cl(unsigned char *e_ident);
+void p_data(unsigned char *e_ident);
+void p_vers(unsigned char *e_ident);
+void p_abi(unsigned char *e_ident);
+void p_osab(unsigned char *e_ident);
+void p_type(unsigned int e_type, unsigned char *e_ident);
+void p_entr(unsigned long int e_entry, unsigned char *e_ident);
 void c_elfile(int elf);
 
 /**
  * validate_elf - Validates if a file follows the ELF format.
- * @elf_id: Pointer to an array containing the ELF magic numbers.
+ * @e_ident: Pointer to an array containing the ELF magic numbers.
  *
  * Description:
  * This function checks whether a given file adheres to the ELF (Executable and Linkable Format) standards.
  * If the file is not recognized as an ELF file, the program exits with error code 98.
  */
 
-void validate_elf(unsigned char *elf_id)
+void validate_elf(unsigned char *e_ident)
 {
 	int i;
 
 	for (i = 0; i < 4; i++)
 	{
-		if (elf_id[i] != 127 &&
-		    elf_id[i] != 'E' &&
-		    elf_id[i] != 'L' &&
-		    elf_id[i] != 'F')
+		if (e_ident[i] != 127 &&
+		    e_ident[i] != 'E' &&
+		    e_ident[i] != 'L' &&
+		    e_ident[i] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
@@ -45,14 +45,14 @@ void validate_elf(unsigned char *elf_id)
 
 /**
  * p_mag - Prints the magic numbers of an ELF header.
- * @elf_id: Pointer to an array holding the ELF magic numbers.
+ * @e_ident: Pointer to an array holding the ELF magic numbers.
  *
  * Description:
  * This function prints the magic numbers from the provided ELF header array.
  * The magic numbers are separated by spaces in the output.
  */
 
-void p_mag(unsigned char *elf_id)
+void p_mag(unsigned char *e_ident)
 {
 	int i;
 
@@ -60,7 +60,7 @@ void p_mag(unsigned char *elf_id)
 
 	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x", elf_id[i]);
+		printf("%02x", e_ident[i]);
 
 		if (i == EI_NIDENT - 1)
 			printf("\n");
@@ -71,18 +71,18 @@ void p_mag(unsigned char *elf_id)
 
 /**
  * p_cl - Prints the class of an ELF header.
- * @elf_id: Pointer to an array containing the ELF class.
+ * @e_ident: Pointer to an array containing the ELF class.
  *
  * Description:
  * This function prints the ELF class value from the provided array in the ELF header.
  * The ELF class indicates whether the ELF file is 32-bit or 64-bit.
  */
 
-void p_cl(unsigned char *elf_id)
+void p_cl(unsigned char *e_ident)
 {
 	printf("  Class:                             ");
 
-	switch (elf_id[EI_CLASS])
+	switch (e_ident[EI_CLASS])
 	{
 	case ELFCLASSNONE:
 		printf("none\n");
@@ -94,24 +94,24 @@ void p_cl(unsigned char *elf_id)
 		printf("ELF64\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", elf_id[EI_CLASS]);
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
 /**
  * p_data - Prints the data encoding of an ELF header.
- * @elf_id: Pointer to an array containing the ELF data encoding.
+ * @e_ident: Pointer to an array containing the ELF data encoding.
  *
  * Description:
  * This function prints the data encoding value from the provided array in the ELF header.
  * The data encoding specifies the endianness of the ELF file's data representation.
  */
 
-void p_data(unsigned char *elf_id)
+void p_data(unsigned char *e_ident)
 {
 	printf("  Data:                              ");
 
-	switch (elf_id[EI_DATA])
+	switch (e_ident[EI_DATA])
 	{
 	case ELFDATANONE:
 		printf("none\n");
@@ -123,25 +123,25 @@ void p_data(unsigned char *elf_id)
 		printf("2's complement, big endian\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", elf_id[EI_CLASS]);
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
 /**
  * p_vers - Prints the version of an ELF header.
- * @elf_id: Pointer to an array containing the ELF version.
+ * @e_ident: Pointer to an array containing the ELF version.
  *
  * Description:
  * This function prints the version value from the provided array in the ELF header.
  * The version indicates the version of the ELF file format being used.
  */
 
-void p_vers(unsigned char *elf_id)
+void p_vers(unsigned char *e_ident)
 {
 	printf("  Version:                           %d",
-	       elf_id[EI_VERSION]);
+	       e_ident[EI_VERSION]);
 
-	switch (elf_id[EI_VERSION])
+	switch (e_ident[EI_VERSION])
 	{
 	case EV_CURRENT:
 		printf(" (current)\n");
@@ -154,18 +154,18 @@ void p_vers(unsigned char *elf_id)
 
 /**
  * p_osab - Prints the OS/ABI of an ELF header.
- * @elf_id: Pointer to an array containing the ELF OS/ABI.
+ * @e_ident: Pointer to an array containing the ELF OS/ABI.
  *
  * Description:
  * This function prints the OS/ABI value from the provided array in the ELF header.
  * The OS/ABI value identifies the operating system and ABI (Application Binary Interface) for which the ELF file is intended.
  */
 
-void p_osab(unsigned char *elf_id)
+void p_osab(unsigned char *e_ident)
 {
 	printf("  OS/ABI:                            ");
 
-	switch (elf_id[EI_OSABI])
+	switch (e_ident[EI_OSABI])
 	{
 	case ELFOSABI_NONE:
 		printf("UNIX - System V\n");
@@ -198,43 +198,43 @@ void p_osab(unsigned char *elf_id)
 		printf("Standalone App\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", elf_id[EI_OSABI]);
+		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
 }
 
 /**
  * p_abi - Prints the ABI version of an ELF header.
- * @elf_id: Pointer to an array containing the ELF ABI version.
+ * @e_ident: Pointer to an array containing the ELF ABI version.
  *
  * Description:
  * This function prints the ABI (Application Binary Interface) version value from the provided array in the ELF header.
  * The ABI version indicates the version of the ABI for which the ELF file is compiled.
  */
 
-void p_abi(unsigned char *elf_id)
+void p_abi(unsigned char *e_ident)
 {
 	printf("  ABI Version:                       %d\n",
-	       elf_id[EI_ABIVERSION]);
+	       e_ident[EI_ABIVERSION]);
 }
 
 /**
  * p_type - Prints the type of an ELF header.
- * @elf_type: The ELF type.
- * @elf_id: Pointer to an array containing the ELF class.
+ * @e_type: The ELF type.
+ * @e_ident: Pointer to an array containing the ELF class.
  *
  * Description:
- * This function prints the ELF type value provided as 'elf_type' along with the ELF class from the array in the ELF header.
+ * This function prints the ELF type value provided as 'e_type' along with the ELF class from the array in the ELF header.
  * The ELF type indicates the purpose and nature of the ELF file.
  */
 
-void p_type(unsigned int elf_type, unsigned char *elf_id)
+void p_type(unsigned int e_type, unsigned char *e_ident)
 {
-	if (elf_id[EI_DATA] == ELFDATA2MSB)
-		elf_type >>= 8;
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type >>= 8;
 
 	printf("  Type:                              ");
 
-	switch (elf_type)
+	switch (e_type)
 	{
 	case ET_NONE:
 		printf("NONE (None)\n");
@@ -252,36 +252,36 @@ void p_type(unsigned int elf_type, unsigned char *elf_id)
 		printf("CORE (Core file)\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", elf_type);
+		printf("<unknown: %x>\n", e_type);
 	}
 }
 
 /**
  * p_entr - Prints the entry point of an ELF header.
- * @elf_ent: The address of the ELF entry point.
- * @elf_id: Pointer to an array containing the ELF class.
+ * @e_entry: The address of the ELF entry point.
+ * @e_ident: Pointer to an array containing the ELF class.
  *
  * Description:
- * This function prints the entry point address provided as 'elf_ent' along with the ELF class from the array in the ELF header.
+ * This function prints the entry point address provided as 'e_entry' along with the ELF class from the array in the ELF header.
  * The entry point is the memory address where the execution of the ELF file begins.
  */
 
-void p_entr(unsigned long int elf_ent, unsigned char *elf_id)
+void p_entr(unsigned long int e_entry, unsigned char *e_ident)
 {
 	printf("  Entry point address:               ");
 
-	if (elf_id[EI_DATA] == ELFDATA2MSB)
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
-		elf_ent = ((elf_ent << 8) & 0xFF00FF00) |
-			  ((elf_ent >> 8) & 0xFF00FF);
-		elf_ent = (elf_ent << 16) | (elf_ent >> 16);
+		e_entry = ((e_entry << 8) & 0xFF00FF00) |
+			  ((e_entry >> 8) & 0xFF00FF);
+		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 
-	if (elf_id[EI_CLASS] == ELFCLASS32)
-		printf("%#x\n", (unsigned int)elf_ent);
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+		printf("%#x\n", (unsigned int)e_entry);
 
 	else
-		printf("%#lx\n", elf_ent);
+		printf("%#lx\n", e_entry);
 }
 
 /**
@@ -343,16 +343,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	validate_elf(heder->elf_id);
+	validate_elf(heder->e_ident);
 	printf("ELF Header:\n");
-	p_mag(heder->elf_id);
-	p_cl(heder->elf_id);
-	p_data(heder->elf_id);
-	p_vers(heder->elf_id);
-	p_osab(heder->elf_id);
-	p_abi(heder->elf_id);
-	p_type(heder->elf_type, heder->elf_id);
-	p_entr(heder->elf_ent, heder->elf_id);
+	p_mag(heder->e_ident);
+	p_cl(heder->e_ident);
+	p_data(heder->e_ident);
+	p_vers(heder->e_ident);
+	p_osab(heder->e_ident);
+	p_abi(heder->e_ident);
+	p_type(heder->e_type, heder->e_ident);
+	p_entr(heder->e_entry, heder->e_ident);
 
 	free(heder);
 	c_elfile(o);
