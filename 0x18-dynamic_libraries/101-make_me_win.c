7a391a8e40917bb86c69fc3fec531bf32e8b848f
kamel <kamelabouchaala@gmail.com>
2023-10-16 14:55:09 -0700

    add all

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
-int main(int argc, char **argv) {
-    int (*real_main)(int, char **) = dlsym(RTLD_NEXT, "main");
-    printf("Please make me win!\n");
-    return 0;
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int (*real_main)(int, char **) = dlsym(RTLD_NEXT, "main");
+	printf("Please make me win!\n");
+	return 0;
 }
