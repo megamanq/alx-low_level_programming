3446196d6082f26527dc3c5182d1795b6926b3cd
kamel <kamelabouchaala@gmail.com>
2023-10-24 13:30:53 -0700

    add 0. Print listé

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all
+ * the elements of a dlistint_t list
+ * @h: header ptr
+ * eturn: nbr of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	int i;
+
+	if (!h)
+	{
+		return (0);
+	}
+
+	temp = h;
+	i = 0;
+
+	while (temp)
+	{
+		printf("%i\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
