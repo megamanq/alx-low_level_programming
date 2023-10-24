afb98e400c9fe8f408537853f5d10e1e0f990cf1
kamel <kamelabouchaala@gmail.com>
2023-10-24 14:31:08 -0700

    fix 2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,6 +21,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
+
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
+
 	head = &new;
 	return (*head);
 }
