ca7d5071d86ac34c960d5463ba3339bf5371f7ff
kamel <kamelabouchaala@gmail.com>
2023-10-25 09:24:47 -0700

    5. Get node at index f

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -23,6 +23,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	for (i = 1; i < index; i++)
 	{
 		temp = temp->next;
+		if (!temp)
+		{
+			return (NULL);
+		}
 	}
 	return (temp);
 }
