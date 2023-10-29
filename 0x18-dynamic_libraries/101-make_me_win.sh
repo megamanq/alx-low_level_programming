66f509bb6cfea1bd02922885a421ff793d5b8680
kamel <kamelabouchaala@gmail.com>
2023-10-29 11:42:56 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P  https://github.com/megamanq/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.so
-LD_PRELOAD=/tmp/101-make_me_win.so gm
+wget -P /tmp https://github.com/megamanq/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.so
+export LD_PRELOAD=/tmp/101-make_me_win.so gm
