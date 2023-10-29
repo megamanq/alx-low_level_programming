d2313f8e49892daebaad2538c3aecd1cb1f1785c
kamel <kamelabouchaala@gmail.com>
2023-10-29 10:48:50 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/f1.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int _putchar(char c)
+{return 1;}
+int _islower(int c)
+{return 1;}
+int _isalpha(int c)
+{return 1;}
+int _abs(int n)
+{return 1;}
+int _isupper(int c)
+{return 1;}
+int _isdigit(int c)
+{return 1;}
+int _strlen(char *s)
+{return 1;}
+int _atoi(char *s)
+{return 1;}
+int _strcmp(char *s1, char *s2)
+{return 1;}
+unsigned int _strspn(char *s, char *accept)
+{return 1;}
---
+++ 0x18-dynamic_libraries/f2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void _puts(char *s)
+{}
+char *_strcpy(char *dest, char *src)
+{return "c";}
+char *_strcat(char *dest, char *src)
+{return "c";}
+char *_strncat(char *dest, char *src, int n)
+{return "c";}
+char *_strncpy(char *dest, char *src, int n)
+{return "c";}
+char *_memset(char *s, char b, unsigned int n)
+{return "c";}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{return "c";}
+char *_strchr(char *s, char c)
+{return "c";}
+char *_strpbrk(char *s, char *accept)
+{return "c";}
+char *_strstr(char *haystack, char *needle)
+{return "c";}
