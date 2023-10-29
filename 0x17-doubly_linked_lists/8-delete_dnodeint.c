18563a0babaaa5df5e3eeef164f87e64c5a311dc
kamel <kamelabouchaala@gmail.com>
2023-10-29 08:26:27 -0700

    8. Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,48 +14,32 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int len;
 
 	if (!(*head))
-	{
 		return (-1);
-	}
-
 	temp = *head;
-
 	if (index == 0)
 	{
 		*head = temp->next;
 		if (temp->next)
-		{
 			temp->next->prev = NULL;
-		}
 		free(temp);
 		return (1);
 	}
-
 	len = 0;
-
 	while (temp && len < index)
 	{
 		temp = temp->next;
 		len++;
 	}
-
 	if (!temp)
-	{
-		return(-1);
-	}
-
+		return (-1);
 	if (len == index)
 	{
-		if(temp->prev)
-		{
+		if (temp->prev)
 			temp->prev->next = temp->next;
-		}
 		if (temp->next)
-		{
 			temp->next->prev = temp->prev;
-		}
 		free(temp);
-		return(1);
+		return (1);
 	}
 	return (-1);
 }
