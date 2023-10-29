73f6a38fd5a07280f2087321110977a9508a24f9
kamel <kamelabouchaala@gmail.com>
2023-10-29 11:48:30 -0700

    add files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/megamanq/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.so
-export LD_PRELOAD=/tmp/101-make_me_win.so gm
+wget -P /tmp https://github.com/megamanq/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so gm
